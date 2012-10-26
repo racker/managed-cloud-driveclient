6ff9c7603d49624a775bb620e8a59f043073b4de
BK Box <bk.box@rackspace.com>
2012-10-26 12:50:03 -0500

    Use apt and yum cookbooks

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,12 @@ maintainer_email "bk.box@rackspace.com"
 license          "Apache 2.0"
 description      "Installs/Configures driveclient"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "1.0.0"
 
-%w{redhat centos ubuntu}.each do |os|
+%w{ redhat centos ubuntu }.each do |os|
   supports os
+end
+
+%w{ apt yum }.each do |cb|
+  depends cb
 end
